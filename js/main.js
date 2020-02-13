546ec17942ba1a4827f9db0c8a37d05902faf438
unknown <vadiksimf88@gmail.com>
2020-02-13 16:40:19 +0300

    Add content

--- js/main.js
+++ js/main.js
@@ -1,13 +1,33 @@
 /*new WOW().init();*/
 
 var mySwiper = new Swiper('.swiper-container', {
-    spaceBetween: 100,
-    slidesPerView: 2,
+    //spaceBetween: 100,
+    slidesPerView: 1,
     loop: true,
     navigation: {
       nextEl: '.btn-nav'
     },
+    breakpoints: {
+      540: {
+        slidesPerView: 2
+      }
+  }
 });
+
+var menuButton = document.querySelector('.burger');
+var menu = document.querySelector('nav');
+menuButton.addEventListener('click', function () {
+console.log('Click')
+menuButton.classList.toggle('burger-active');
+menu.classList.toggle('nav-active');
+});
+
+
+/*.addEventListener('click', function () {
+  console.log('Click!')
+  menuButton.classList.toggle('burger-active');
+  menuButton.classList.toggle('nav-active');
+})*/
 
 /*
 var mySwiper = new Swiper ('.swiper-container', {
